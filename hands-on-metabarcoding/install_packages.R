7a624fc2e32737ab893e1830edfad1b72958b1e0
Francisco Pascoal <fpascoal1996@gmail.com>
2026-04-30 14:22:35 +0100

    Add R packages install instructions

---
+++ hands-on-metabarcoding/install_packages.R
@@ -0,0 +1,17 @@
+# Install packages
+install.packages("dplyr", dependencies = TRUE)
+install.packages("tidyr", dependencies = TRUE)
+install.packages("stringi", dependencies = TRUE)
+install.packages("stringr", dependencies = TRUE)
+install.packages("ggplot2", dependencies = TRUE)
+install.packages("vegan", dependencies = TRUE)
+install.packages("ulrb", dependencies = TRUE)
+install.packages("readxl", dependencies = TRUE)
+install.packages("ggVennDiagram", dependencies = TRUE)
+
+# Install DADA2 (https://benjjneb.github.io/dada2/dada-installation.html)
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("dada2", version = "3.11")
+
+# if this doesn't work check the tutorial page for other install options
