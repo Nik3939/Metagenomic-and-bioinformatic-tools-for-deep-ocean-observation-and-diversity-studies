f67c79097cbceb8fc836ff5fc18144907da256c2
Francisco Pascoal <fpascoal1996@gmail.com>
2026-05-06 15:26:43 +0100

    Add upstream materials

--- hands-on-metabarcoding/install_packages.R
+++ hands-on-metabarcoding/install_packages.R
@@ -12,6 +12,6 @@ install.packages("ggVennDiagram", dependencies = TRUE)
 # Install DADA2 (https://benjjneb.github.io/dada2/dada-installation.html)
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install("dada2", version = "3.22") # note: you may need to change version depending on your R version (this one works for R 4.5)
+BiocManager::install("dada2", version = "3.11")
 
 # if this doesn't work check the tutorial page for other install options
